44a72fadfa943f60d2a854e9654fa32fba32a23f
ipedevi <github@delviso.net>
2016-11-24 17:59:39 +0100

    Inicial

---
+++ devopsday/src/main/java/com/sg/devopsday/App.java
@@ -0,0 +1,13 @@
+package com.sg.devopsday;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hola Mundo Mundial!" );
+    }
+}
